dc7a60f73371a0a1693b7d3aaa8cdf025d71f261
jmmarino <lostcanadain@gmail.com>
2015-11-24 23:23:09 -0500

    Update stop_time.py

--- gtfsdb/model/stop_time.py
+++ gtfsdb/model/stop_time.py
@@ -17,16 +17,16 @@ class StopTime(Base):
 
     __tablename__ = 'stop_times'
 
-    trip_id = Column(String(255), primary_key=True, index=True, nullable=False)
+    trip_id = Column(String(255), primary_key=True,nullable=False)
     arrival_time = Column(String(9))
-    departure_time = Column(String(9), index=True)
-    stop_id = Column(String(255),nullable=False)
-    stop_sequence = Column(Integer, primary_key=True, nullable=False)
+    departure_time = Column(String(9))
+    stop_id = Column(String(255), nullable=False)
+    stop_sequence = Column(Integer)
     stop_headsign = Column(String(255))
     pickup_type = Column(Integer, default=0)
     drop_off_type = Column(Integer, default=0)
     shape_dist_traveled = Column(Numeric(20, 10))
-    timepoint = Column(Boolean, index=True, default=False)
+    timepoint = Column(Boolean,default=False)
 
     stop = relationship(
         'Stop',
